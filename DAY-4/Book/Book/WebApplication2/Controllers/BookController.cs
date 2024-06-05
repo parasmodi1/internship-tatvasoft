41d03a206b94cc134811c44d758542b4488bb177
parasmodi1 <parasmodi24103@gmail.com>
2024-06-06 03:05:20 +0530

    first commit

---
+++ DAY-4/Book/Book/WebApplication2/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using DataLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer;
+using ServiceLayer.Model;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBook()
+        {
+            var result = _bookService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("GetBookById")]
+        public IActionResult GetBookById(int id)
+        {
+            var result = _bookService.GetById(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertBook(BookDetails book)
+        {
+            await _bookService.InsertBookDetails(book);
+            return Ok();
+        }
+    }
+}
