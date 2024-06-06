2f73f34f4341a2e01549e817d4462f3a103c80df
parasmodi1 <parasmodi24103@gmail.com>
2024-06-07 00:33:56 +0530

    first commit

---
+++ Day-7/CIPlatfromWebAPI/Controllers/AdminUserController.cs
@@ -0,0 +1,32 @@
+﻿using Business_logic_Layer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminUserController : ControllerBase
+    {
+        private readonly BALAdminUser _balAdminUser;
+
+        public AdminUserController(BALAdminUser balAdminUser)
+        {
+            _balAdminUser = balAdminUser;
+        }
+
+        [HttpGet("UserDetailList")]
+        public async Task<IActionResult> GetUserDetailList()
+        {
+            try
+            {
+                var userDetailList = await _balAdminUser.UserDetailsAsync();
+                return Ok(userDetailList);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
